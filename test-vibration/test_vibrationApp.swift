ed400f6b60fe0dba50f1d7cb8e087dc7b96e5fed
Fuuma <masa81hiro69@gmail.com>
2025-06-18 00:59:02 +0900

    Initial Commit

---
+++ test-vibration/test_vibrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_vibrationApp.swift
+//  test-vibration
+//
+//  Created by hiro on 2025/06/18.
+//
+
+import SwiftUI
+
+@main
+struct test_vibrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
